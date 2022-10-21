1d5a94bc8546e9e7ccf689b0b9cb1adfe2ed5269
shakyarl <shakyarl@mail.uc.edu>
2022-10-20 23:34:02 -0400

    Add firebase, mention components, and start header

--- src/App.js
+++ src/App.js
@@ -5,6 +5,13 @@ function App() {
     // BEM naming convetion
     <div className="app">
       <h1>Lets build the facebook clone</h1>
+
+      {/* Header */}
+
+      {/* App Body */}
+        {/* Side Bar */}
+        {/* Feed */}
+        {/* Widgets */}
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA64j3B2BV6xgo16viQopqWNazz9WJ-Bg8",
+  authDomain: "facebook-clone-d7f2c.firebaseapp.com",
+  projectId: "facebook-clone-d7f2c",
+  storageBucket: "facebook-clone-d7f2c.appspot.com",
+  messagingSenderId: "135047961443",
+  appId: "1:135047961443:web:a467a7af8c0dcb5bf29156",
+  measurementId: "G-CPKQ34YFEM"
+};
