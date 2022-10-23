f7e9cd8340bc1896515fa9049496047f526c0e0d
shakyarl <shakyarl@mail.uc.edu>
2022-10-22 21:33:35 -0400

    Story Reel styling

---
+++ src/StoryReel.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import './StoryReel.css'
+import Story from './Story'
+
+function StoryReel() {
+  return (
+    <div className='storyReel'>
+        <Story
+            image="https://scontent.fluk1-1.fna.fbcdn.net/v/t1.18169-9/14522821_587175708153167_6354002634555991139_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=cpBTIp97jT0AX9pQBrm&_nc_oc=AQl5l6uGXKQz9AMYI15Wanya7XhvecTnmE44xsjTSpTR-MV94Eq-UtiprtpjJ9Hjvf5h_Q53XHPvzs3wgYp9HVBp&_nc_ht=scontent.fluk1-1.fna&oh=00_AT8CQnIuND3OjJ9jkxC21phX9ogNnzRsQW2h9IxG7_aRRw&oe=6379EB27" 
+            profileSrc="https://scontent.fosu2-1.fna.fbcdn.net/v/t39.30808-6/309037611_434412132123157_9090134691965249742_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=iw7jJhXJZO8AX-Gyyko&_nc_ht=scontent.fosu2-1.fna&oh=00_AT8BhawKwi1NfZd3df7Om6cJhDghTF_TGrzeE8FbIZnb4Q&oe=6359171E"
+            title = "Rahul Shakya"
+        />
+        <Story
+            image="https://scontent.fluk1-1.fna.fbcdn.net/v/t1.18169-9/14522821_587175708153167_6354002634555991139_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=cpBTIp97jT0AX9pQBrm&_nc_oc=AQl5l6uGXKQz9AMYI15Wanya7XhvecTnmE44xsjTSpTR-MV94Eq-UtiprtpjJ9Hjvf5h_Q53XHPvzs3wgYp9HVBp&_nc_ht=scontent.fluk1-1.fna&oh=00_AT8CQnIuND3OjJ9jkxC21phX9ogNnzRsQW2h9IxG7_aRRw&oe=6379EB27" 
+            profileSrc="https://scontent.fosu2-1.fna.fbcdn.net/v/t39.30808-6/309037611_434412132123157_9090134691965249742_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=iw7jJhXJZO8AX-Gyyko&_nc_ht=scontent.fosu2-1.fna&oh=00_AT8BhawKwi1NfZd3df7Om6cJhDghTF_TGrzeE8FbIZnb4Q&oe=6359171E"
+            title = "Rahul Shakya"
+        />  
+        <Story
+            image="https://scontent.fluk1-1.fna.fbcdn.net/v/t1.18169-9/14522821_587175708153167_6354002634555991139_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=cpBTIp97jT0AX9pQBrm&_nc_oc=AQl5l6uGXKQz9AMYI15Wanya7XhvecTnmE44xsjTSpTR-MV94Eq-UtiprtpjJ9Hjvf5h_Q53XHPvzs3wgYp9HVBp&_nc_ht=scontent.fluk1-1.fna&oh=00_AT8CQnIuND3OjJ9jkxC21phX9ogNnzRsQW2h9IxG7_aRRw&oe=6379EB27" 
+            profileSrc="https://scontent.fosu2-1.fna.fbcdn.net/v/t39.30808-6/309037611_434412132123157_9090134691965249742_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=iw7jJhXJZO8AX-Gyyko&_nc_ht=scontent.fosu2-1.fna&oh=00_AT8BhawKwi1NfZd3df7Om6cJhDghTF_TGrzeE8FbIZnb4Q&oe=6359171E"
+            title = "Rahul Shakya"
+        />
+        <Story
+            image="https://scontent.fluk1-1.fna.fbcdn.net/v/t1.18169-9/14522821_587175708153167_6354002634555991139_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=cpBTIp97jT0AX9pQBrm&_nc_oc=AQl5l6uGXKQz9AMYI15Wanya7XhvecTnmE44xsjTSpTR-MV94Eq-UtiprtpjJ9Hjvf5h_Q53XHPvzs3wgYp9HVBp&_nc_ht=scontent.fluk1-1.fna&oh=00_AT8CQnIuND3OjJ9jkxC21phX9ogNnzRsQW2h9IxG7_aRRw&oe=6379EB27" 
+            profileSrc="https://scontent.fosu2-1.fna.fbcdn.net/v/t39.30808-6/309037611_434412132123157_9090134691965249742_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=iw7jJhXJZO8AX-Gyyko&_nc_ht=scontent.fosu2-1.fna&oh=00_AT8BhawKwi1NfZd3df7Om6cJhDghTF_TGrzeE8FbIZnb4Q&oe=6359171E"
+            title = "Rahul Shakya"
+        />
+        <Story
+            image="https://scontent.fluk1-1.fna.fbcdn.net/v/t1.18169-9/14522821_587175708153167_6354002634555991139_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=cpBTIp97jT0AX9pQBrm&_nc_oc=AQl5l6uGXKQz9AMYI15Wanya7XhvecTnmE44xsjTSpTR-MV94Eq-UtiprtpjJ9Hjvf5h_Q53XHPvzs3wgYp9HVBp&_nc_ht=scontent.fluk1-1.fna&oh=00_AT8CQnIuND3OjJ9jkxC21phX9ogNnzRsQW2h9IxG7_aRRw&oe=6379EB27" 
+            profileSrc="https://scontent.fosu2-1.fna.fbcdn.net/v/t39.30808-6/309037611_434412132123157_9090134691965249742_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=iw7jJhXJZO8AX-Gyyko&_nc_ht=scontent.fosu2-1.fna&oh=00_AT8BhawKwi1NfZd3df7Om6cJhDghTF_TGrzeE8FbIZnb4Q&oe=6359171E"
+            title = "Rahul Shakya"
+        />
+    </div>
+  )
+}
+
+export default StoryReel
