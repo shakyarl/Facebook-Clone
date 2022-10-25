29b092ae8ca57fc237601869abfe20bfb618d649
shakyarl <shakyarl@mail.uc.edu>
2022-10-25 14:07:12 -0400

    add firebase config info

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,7 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore/lite'
+import { getAuth, GoogleAuthProvider } from "firebase/auth"; 
+
 const firebaseConfig = {
   apiKey: "AIzaSyA64j3B2BV6xgo16viQopqWNazz9WJ-Bg8",
   authDomain: "facebook-clone-d7f2c.firebaseapp.com",
@@ -7,3 +11,13 @@ const firebaseConfig = {
   appId: "1:135047961443:web:a467a7af8c0dcb5bf29156",
   measurementId: "G-CPKQ34YFEM"
 };
+
+
+//from this part on, it might be difficult to connect, because it didn't work last time
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+export { auth, db };
+export default provider;
