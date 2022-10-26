c5dc750632487c22f2ed221c41802d0918bf6487
shakyarl <shakyarl@mail.uc.edu>
2022-10-25 23:21:41 -0400

    add more avatar connection, edit feed styling

--- src/MessageSender.js
+++ src/MessageSender.js
@@ -3,8 +3,10 @@ import { Avatar } from '@mui/material'
 import React from 'react'
 import { useState } from 'react';
 import './MessageSender.css'
+import { useStateValue } from './StateProvider'
 
 function MessageSender() {
+    const [{ user }, dispatch] = useStateValue();
     const [input, setInput] = useState("");
     const [imageUrl, setImageUrl] = useState("");
 
@@ -20,13 +22,13 @@ function MessageSender() {
     return (
         <div className = 'messageSender'>
             <div className="messageSender__top">
-                <Avatar />
+                <Avatar src={user.photoURL} />
                 <form>
                     <input 
                         value={input}
                         onChange={(e) => setInput(e.target.value)}
                         className='messageSender__input' 
-                        placeholder={`What's on your mind?`} />
+                        placeholder={`What's on your mind, ${user.displayName}?`} />
                     <input
                         placeholder='Image URL (Optional)'
                 />
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -2,11 +2,14 @@ import { Chat, EmojiFlags, ExpandMoreOutlined, LocalHospital, People, Storefront
 import React from 'react'
 import './Sidebar.css';
 import SidebarRow from './SidebarRow';
+import { useStateValue } from './StateProvider'
 
 function Sidebar() {
+    const [{ user }, dispatch] = useStateValue();
+
     return <div className='sidebar'>
-        <SidebarRow src= 'https://scontent.fluk1-1.fna.fbcdn.net/v/t1.18169-9/14522821_587175708153167_6354002634555991139_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=cpBTIp97jT0AX9pQBrm&_nc_oc=AQl5l6uGXKQz9AMYI15Wanya7XhvecTnmE44xsjTSpTR-MV94Eq-UtiprtpjJ9Hjvf5h_Q53XHPvzs3wgYp9HVBp&_nc_ht=scontent.fluk1-1.fna&oh=00_AT8CQnIuND3OjJ9jkxC21phX9ogNnzRsQW2h9IxG7_aRRw&oe=6379EB27'
-            title='Rahul Shakya' />
+        <SidebarRow src= {user.photoURL}
+            title={user.displayName} />
         <SidebarRow Icon={LocalHospital}
             title ='Covid-19 Information Center' />
         <SidebarRow Icon={EmojiFlags}
