3fee91b14cbe56b4135c3af80ac192afffecf1ad
shakyarl <shakyarl@mail.uc.edu>
2022-10-25 23:10:13 -0400

    avatar picture load works

--- src/Header.js
+++ src/Header.js
@@ -11,8 +11,13 @@ import AddIcon from '@mui/icons-material/Add';
 import ForumIcon from '@mui/icons-material/Forum';
 import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { useStateValue } from './StateProvider'
 
 function Header() {
+    const [{ user }, dispatch] = useStateValue();
+
+    
+
     return <div className="header">
         <div className="header__left">
             <img
@@ -44,8 +49,8 @@ function Header() {
         </div>
         <div className="header__right">
             <div className="header__info">
-                <Avatar />
-                <h4>raraheno</h4>
+                <Avatar src={user.photoURL} />
+                <h4>{ user.displayName}</h4>
             </div>
 
             <IconButton>
